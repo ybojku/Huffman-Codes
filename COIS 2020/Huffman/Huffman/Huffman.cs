7be72b34d08d8c33d89bed27c37d31828a88f41b
yllkabojku <46692812+yllkabojku@users.noreply.github.com>
2020-11-09 08:33:22 -0500

    Add files via upload

---
+++ COIS 2020/Huffman/Huffman/Huffman.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using PriorityQueue;
+/// <summary>
+/// This is assignment 2. COIS 2020H. Huffman.
+/// 
+/// authors: Yllka Bokju and  Sam ...
+/// </summary>
+namespace HuffmanProgram
+{
+    //-----------------------------------------------------------------------
+    /// <summary>
+    /// Class name: Node
+    /// description: creates a node for the tree. it also implements CompareTo method.
+    /// </summary>
+    public class Node : IComparable
+    {
+        #region Field
+
+        public char Character { get; set; }
+        public int Frequency { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        #endregion
+
+        #region Constructor
+        public Node(char character, int frequency, Node left, Node right)
+        {
+            Character = character;
+            Frequency = frequency;
+            Left = left;
+            Right = right;
+        }
+        #endregion
+
+        #region CompareTo
+        //Returns >0 if the current node has more frequency than obj
+        //Returns 0 if the current node is equal to obj
+        //Returns <0 if the current node is less than obj
+        public int CompareTo(Object obj)
+        {
+            if (obj != null)
+            {
+                Node other = (Node)obj;   // Explicit cast
+                if (other != null)
+                    return Frequency - other.Frequency;
+                else
+                    return 1;
+            }
+            else
+                return 1;
+        }
+        #endregion
+    }
+
+    //------------------------------------------------------------------------
+    /// <summary>
+    /// Class name: Huffman
+    /// description: This class creates a binary tree using priority queue. It takes the string entered by the user and ecodes 
+    ///              it into 1's and 0's. which can also decode the 1's and 0's to string.
+    /// </summary>
+    public class Huffman
+    {
+        #region Field
+        private Node HT; // Huffman tree to create codes and decode text
+        private Dictionary<char, string> D; // Dictionary to encode text
+        #endregion
+
+        #region Constructor
+        public Huffman(string S) 
+        {
+            D = new Dictionary<char, string>();   //create dictionary 
+            Build(AnalyzeText(S));                //builds the binary heap
+            CreateCodes();                        //creates code for the binary heap
+
+        }
+        #endregion
+
+        #region AnalyzeText Method
+        // 15 marks
+        // Return the frequency of each character in the given text (invoked by Huffman)
+        // The method first removes all the characters that are repeated in the string to remain with unique characters.
+        // this is stored in variable unique.
+        // it also initializes an array of size twice the length of unique (unique.Length x 2), with name frequency.
+        // the array frequency stores each character with its frequency.
+        // for example for the word: aaabbcdf a -> has frequecy of 3, b -> has 2, c -> 1, d -> 1 and f-> 1
+        // this can be stored in frequency array as:         {['a'],[3],['b'],[2],['c'],[1],['d'],[1],['f'],[1]}
+        // the array keeps characters in even indices and frequencies in odd indices.
+        // it also removes all the none letter characters, for simplicity 
+        private int[] AnalyzeText(string S) 
+        {
+            
+            S = Regex.Replace(S, "[^a-zA-Z, ]", String.Empty);  //removes all the non letter characters except space.
+            var unique = new HashSet<char>(S);                //removes the duplicate charactes
+            int[] frequency = new int[unique.Count*2];        //creates an array of size number of unique characters in the string
+            int i = 0, j = 1;
+
+
+            // time complexity O(n ^ 2).
+            foreach (char item in unique)  //go through each character in unique
+            {
+                frequency[i] = (int)item;
+
+                foreach(char c in S)
+                {
+                    if (item == c)
+                        frequency[j]++;
+                }
+                //iterate
+                i += 2;    // moves to next even number                 
+                j += 2;    // moves to next odd number
+            }
+
+            return frequency;            //return the array
+        }
+        #endregion
+
+        #region Build
+        // 20 marks
+        // Build a Huffman tree based on the character frequencies greater than 0 (invoked by Huffman)
+        private void Build(int[] F)
+        {
+            PriorityQueue<Node> PQ;
+            PQ = new PriorityQueue<Node>(F.Length);     //create an instance of the priority queue
+            Node left, right;
+
+
+            //add all the unique character in the priotity queue
+            for (int i = 0, j = 1; i < F.Length; i += 2, j +=2)
+            {
+                PQ.Add(new Node((char)F[i], F[j], null, null));       //add each node to the priority Queue
+            }
+
+            //it checks wether the priority queue has only one node 
+            if (PQ.Size() == 1)
+            {
+                HT = PQ.Front();
+            }
+
+            //builds a binary tree 
+            else
+            {
+                while (PQ.Size() > 1)
+                {
+                    left = PQ.Front();
+                    PQ.Remove();
+                    right = PQ.Front();
+                    PQ.Remove();
+                    PQ.Add(new Node('/', left.Frequency + right.Frequency, left, right));
+                }
+                HT = PQ.Front();
+            }
+
+        }
+        #endregion
+
+        #region CreateCodes method
+        // 20 marks
+        // Create the code of 0s and 1s for each character by traversing the Huffman tree (invoked by Huffman)
+        // Store the codes in Dictionary D using the char as the key
+        private void CreateCodes() 
+        {
+            Node cur = HT;       //root of the tree
+            String bits = "";   //0s and 1s
+
+
+            //invoke the method
+            addBits(cur, bits);
+
+
+            //creating  local recursive method
+            void addBits(Node temp, string bits)
+            {
+                //traverses through the binary tree
+                if (temp.Left != null && temp.Right != null)
+                {
+                    addBits(temp.Left, bits + "0");
+                    addBits(temp.Right, bits + "1");
+                }
+
+                else
+                {
+                    D.Add(temp.Character, bits);      //stores the character and bits in the dictionary
+
+                }
+            }
+        }
+        #endregion
+
+        #region Encode Method
+        // 10 marks
+        // Encode the given text and return a string of 0s and 1s
+        public string Encode(string S) 
+        {
+            string encode = "";
+
+            foreach (char item in S)
+            {
+                char m = (char)item;
+                foreach (KeyValuePair<char, string> val in D)
+                {
+                    if (m == val.Key)
+                        encode = encode + val.Value;
+                }
+            }
+
+            return encode;
+        }
+        #endregion
+
+        #region Decode Method
+        // 10 marks
+        // Decode the given string of 0s and 1s and return the original text
+        public string Decode(string S) 
+        {
+            Node curr = HT;
+            string decode = "";
+
+            foreach (char ch in S)
+            {
+                if (curr.Left == null)
+                {
+                    decode += curr.Character;
+                    curr = HT;
+                }
+                else
+                {
+                    if (ch == '0')
+                        curr = curr.Left;
+                    else
+                        curr = curr.Right;
+                }
+
+                if (curr.Left == null)
+                {
+                    decode += curr.Character;
+                    curr = HT;
+                }
+            }
+
+            return decode;
+        }
+        #endregion
+    }
+
+    //--------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Class name: Test
+    /// description: This class test the huffman class. This ask users to enter a text which will be encoded and decoded
+    /// </summary>
+    public class Test
+    {
+        // main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Huffman program.");
+            Console.WriteLine("Enter a word: ");
+            string man = Convert.ToString(Console.ReadLine());
+
+
+            Huffman huff = new Huffman(man);  //instance of huffman class 
+
+            String encode = huff.Encode(man);
+            Console.Write("\n Encoded string: ");
+            Console.Write(encode);
+            Console.Write("\n\n\n decoded string: ");
+            Console.WriteLine(huff.Decode(encode));
+           
+
+        }
+    }
+
+}
---
+++ COIS 2020/Huffman/Huffman/PriorityQueue.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priority
+{
+    // Used by class PriorityQueue<T>
+    // Implements IComparable and overrides ToString (from Object)
+
+    public class PriorityClass : IComparable
+    {
+        private int priorityValue;       // Ordered on priorityValue
+        private String name;
+
+        // Constructor
+
+        public PriorityClass(int priority, String name)
+        {
+            this.name = name;
+            priorityValue = priority;
+        }
+
+        // CompareTo (inherited from IComparable)
+        // Returns >0 if the current item is greater than obj (null or otherwise)
+        // Returns 0  if the current item is equal to obj (of PriorityClass)
+        // Returns <0 if the current item is less than obj (of PriorityClass)
+
+        public int CompareTo(Object obj)
+        {
+            if (obj != null)
+            {
+                PriorityClass other = (PriorityClass)obj;   // Explicit cast
+                if (other != null)
+                    return priorityValue - other.priorityValue;
+                else
+                    return 1;
+            }
+            else
+                return 1;
+        }
+
+        // ToString (overridden from Object class)
+        // Returns a string represent of an object of PriorityClass
+
+        public override string ToString()
+        {
+            return name + " with priority " + priorityValue;
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+namespace PriorityQueue
+{
+    using Priority;
+
+     public interface IContainer<T>
+    {
+        void MakeEmpty();  // Reset an instance to empty
+        bool Empty();      // Test if an instance is empty
+        int Size();        // Return the number of items in an instance
+    }
+
+    //-----------------------------------------------------------------------------
+
+    public interface IPriorityQueue<T> : IContainer<T> where T : IComparable
+    {
+        void Add(T item);  // Add an item to a priority queue
+        void Remove();     // Remove the item with the highest priority
+        T Front();         // Return the item with the highest priority
+    }
+
+    //-------------------------------------------------------------------------
+
+    // Priority Queue
+    // Implementation:  Binary heap
+
+    public class PriorityQueue<T> : IPriorityQueue<T> where T : IComparable
+    {
+        private int capacity;  // Maximum number of items in a priority queue
+        private T[] A;         // Array of items
+        private int count;     // Number of items in a priority queue
+
+        // Constructor 1
+        // Create a priority with maximum capacity of size
+        // Time complexity:  O(1)
+
+        public PriorityQueue(int size)
+        {
+            capacity = size;
+            A = new T[size + 1];  // Indexing begins at 1
+            count = 0;
+        }
+
+        // Constructor 2
+        // Create a priority from an array of items
+        // Time complexity:  O(n)
+
+        public PriorityQueue(T[] inputArray)
+        {
+            int i;
+
+            count = capacity = inputArray.Length;
+            A = new T[capacity + 1];
+
+            for (i = 0; i < capacity; i++)
+            {
+                A[i+1] = inputArray[i];
+            }
+
+            BuildHeap();
+        }
+
+        // PercolateUp
+        // Percolate up an item from position i in a priority queue
+        // Time complexity:  O(log n)
+
+        private void PercolateUp(int i)
+        {
+            int child = i, parent;
+
+            // As long as child is not the root (i.e. has a parent)
+            while (child > 1)
+            {
+                parent = child / 2;
+                if (A[child].CompareTo(A[parent]) > 0)
+                // If child has a higher priority than parent
+                {
+                    // Swap parent and child
+                    T item = A[child];
+                    A[child] = A[parent];
+                    A[parent] = item;
+                    child = parent;  // Move up child index to parent index
+                }
+                else
+                    // Item is in its proper position
+                    return;
+            }
+        }
+
+        // Add
+        // Add an item into the priority queue
+        // Time complexity:  O(log n)
+
+        public void Add(T item)
+        {
+            if (count < capacity)
+            {
+                A[++count] = item;  // Place item at the next available position
+                PercolateUp(count);
+            }
+        }
+
+        // PercolateDown
+        // Percolate down from position i in a priority queue
+        // Time complexity:  O(log n)
+
+        private void PercolateDown(int i)
+        {
+            int parent = i, child;
+
+            // while parent has at least one child
+            while (2 * parent <= count)
+            {
+                // Select the child with the highest priority
+                child = 2 * parent;    // Left child index
+                if (child < count)  // Right child also exists
+                    if (A[child + 1].CompareTo(A[child]) > 0)
+                        // Right child has a higher priority than left child
+                        child++;
+
+                // If child has a higher priority than parent
+                if (A[child].CompareTo(A[parent]) > 0)
+                {
+                    // Swap parent and child
+                    T item = A[child];
+                    A[child] = A[parent];
+                    A[parent] = item;
+                    parent = child;  // Move down parent index to child index
+                }
+                else
+                    // Item is in its proper place
+                    return;
+            }
+        }
+
+        // Remove
+        // Remove an item from the priority queue
+        // Time complexity:  O(log n)
+
+        public void Remove()
+        {
+            if (!Empty())
+            {
+                // Remove item with highest priority (root) and
+                // replace it with the last item
+                A[1] = A[count--];
+
+                // Percolate down the new root item
+                PercolateDown(1);
+            }
+        }
+
+        // Front
+        // Return the item with the highest priority
+        // Time complexity:  O(1)
+
+        public T Front()
+        {
+            if (!Empty())
+            {
+                return A[1];  // Return the root item (highest priority)
+            }
+            else
+                return default(T);
+        }
+
+        // BuildHeap
+        // Create a binary heap from a given list
+        // Time complexity:  O(n)
+
+        private void BuildHeap()
+        {
+            int i;
+
+            // Percolate down from the last parent to the root (first parent)
+            for (i = count / 2; i >= 1; i--)
+            {
+                PercolateDown(i);
+            }
+        }
+
+        // HeapSort
+        // Sort and return inputArray
+        // Time complexity:  O(n log n)
+
+        public void HeapSort(T[] inputArray)
+        {
+            int i;
+
+            capacity = count = inputArray.Length;
+
+            // Copy input array to A (indexed from 1)
+            for (i = capacity - 1; i >= 0; i--)
+            {
+                A[i + 1] = inputArray[i];
+            }
+
+            // Create a binary heap
+            BuildHeap();
+
+            // Remove the next item and place it into the input (output) array
+            for (i = 0; i < capacity; i++)
+            {
+                inputArray[i] = Front();
+                Remove();
+            }
+        }
+
+        // MakeEmpty
+        // Reset a priority queue to empty
+        // Time complexity:  O(1)
+
+        public void MakeEmpty()
+        {
+            count = 0;
+        }
+
+        // Empty
+        // Return true if the priority is empty; false otherwise
+        // Time complexity:  O(1)
+
+        public bool Empty()
+        {
+            return count == 0;
+        }
+
+        // Size
+        // Return the number of items in the priority queue
+        // Time complexity:  O(1)
+
+        public int Size()
+        {
+            return count;
+        }
+    }
+
+}
